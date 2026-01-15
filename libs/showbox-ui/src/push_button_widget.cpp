4e58d6b13b77491298eb2557b10a4fc015a3ca80
helton-godoy <chileno.brasil@gmail.com>
2026-01-15 18:54:47 -0400

    feat(ui): implement refactored PushButtonWidget and unit tests

---
+++ libs/showbox-ui/src/push_button_widget.cpp
@@ -0,0 +1,7 @@
+#include "push_button_widget.h"
+
+PushButtonWidget::PushButtonWidget(const QString &text, QWidget *parent)
+    : QPushButton(text, parent)
+{
+    // Default styling or behavior could go here
+}
---
+++ tests/auto/tst_pushbutton.cpp
@@ -0,0 +1,26 @@
+#include <QtTest>
+#include <push_button_widget.h>
+
+class TestPushButton : public QObject
+{
+    Q_OBJECT
+
+private slots:
+    void initTestCase();
+    void testProperties();
+};
+
+void TestPushButton::initTestCase()
+{
+    // Called before the first testfunction is executed
+}
+
+void TestPushButton::testProperties()
+{
+    PushButtonWidget btn("Test Label");
+    QCOMPARE(btn.text(), QString("Test Label"));
+    QVERIFY(btn.isCheckable() == false); // Default
+}
+
+QTEST_MAIN(TestPushButton)
+#include "tst_pushbutton.moc"
